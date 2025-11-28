6c370dccbc05a28deeace5381e668df99dabda36
hyna42 <bylo@duck.com>
2025-11-29 00:39:04 +0100

    use lib

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import requests
+
 name = "amadou"
 print(f"Hello {name}")
 
 
-nb= 42
-print("hyna -> 42 %x" % nb) #entier en hexadécimal minuscule
-print(f"hyna -> 42 : {nb:x}") #entier en hexadécimal avec les f-string
+nb = 42
+print("hyna -> 42 %x" % nb)  # entier en hexadécimal minuscule
+print(f"hyna -> 42 : {nb:x}")  # entier en hexadécimal avec les f-string
 
-price=20.457
-print("Prix: %10.2f" % price) #largeur minimal de 10 caractères, 2 décimal
-print(f"Prix: {price:8.2f}")# meme résultat avec les f-string
+price = 20.457
+print("Prix: %10.2f" % price)  # largeur minimal de 10 caractères, 2 décimal
+print(f"Prix: {price:8.2f}")  # meme résultat avec les f-string
+print("*" * 50)
+response = requests.get("https://www.pyton.org")
+print("Statut de la réponse :", response.status_code)
