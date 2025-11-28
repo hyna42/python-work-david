57da66b1bb099cb181fb1aa67e669bce5d8c8e0a
hyna42 <bylo@duck.com>
2025-11-28 23:17:18 +0100

    formattage

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 name = "amadou"
 print(f"Hello {name}")
+
+
+nb= 42
+print("hyna -> 42 %x" % nb) #entier en hexadécimal minuscule
+print(f"hyna -> 42 : {nb:x}") #entier en hexadécimal avec les f-string
+
+price=20.457
+print("Prix: %10.2f" % price) #largeur minimal de 10 caractères, 2 décimal
+print(f"Prix: {price:8.2f}")# meme résultat avec les f-string
