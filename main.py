410d902862399cf2d6c26555f6a083d39d05d4d7
hyna42 <bylo@duck.com>
2025-11-30 00:27:56 +0100

    exercice 1 et 2 - chapitre 2

--- main.py
+++ main.py
@@ -1,74 +1,46 @@
+from random import randint;
+
 name = "amadou"
 print(f"Hello {name}")
 print("*" * 50)
 
-# FONCTIONS
+
+# EXERCICES FONCTIONS
+# Exercice 1 : Table de multiplication
+#Objectif : afficher la table de multiplication d'un nombre donnée
+
+def table_multiplication(nombre, limite):
+    """Afficher la table de multiplication d'un nombre jusqu'à une limite"""
+    for i in range(1,limite + 1):
+        print(f"{nombre} x {i} = {nombre * i}")
+
+#test
+table_multiplication(7,10)
 
 
-# (1) argument multiples *args / **kwargs
-# *args -->définit un tuple (n,)
-def som(*args):
-    total = 0
-    for i in args:
-        total += i
-    return total
+#Exercice 2 : Jeu du "devine le nombre"
+def jeu_devine_nombre(nombre_secret, max_tentatives):
+    """Deviner un nombre"""
+    tentative = 0
+  
 
-
-nombre = (10, 2, 3, 8)
-print(f"Somme de ({nombre}) = {som(*nombre)}")
-
-
-# **kwargs -> prstock les args dans un dictionnaire clé=valeur
-def afficher_infos(**kwargs):
-    for cle, valeur in kwargs.items():
-        print(f"{cle}: {valeur}")
-
-
-afficher_infos(nom="Justine", age=25, profession="Développeuse")
-
-print("*" * 10 + "fonctions" + "*" * 10)
-# (2) fonction = objet de première classe
-# #en python les fonctions sont des objets de première classe : elle peuvent être assignées à des variables, passées comme argument et retournées par d'autre fonctions
-
-
-def dire_bonjour():
-    return "Bonjour!"
-
-
-def dire_aurevoir():
-    return "Au revoir!"
-
-
-def choisir_salutation(moment):
-    if moment == "matin":
-        return dire_bonjour  # retourn la référence à la fonction sans les ()
+    while( tentative < max_tentatives):
+        try:
+            nombre_devine=int(input("Devinez un nombre entre [0-10]: "))
+            tentative += 1
+            if nombre_devine == nombre_secret:
+                print("Gagné")
+                break
+            elif nombre_devine > nombre_secret:
+                print("Trop grand!")
+            elif nombre_devine < nombre_secret:
+                print("Trop petit !")
+        except ValueError as e:
+            print("Error :",e)
     else:
-        return dire_aurevoir
+        print(f"Perdu ! Le nombre était {nombre_secret}")
+
+jeu_devine_nombre(4,3)
 
 
-# utilisation
-salutation = choisir_salutation("soir")
-print(salutation())
-
-
-# (3) fonction d'ordre supérieur
-def appliquer_operation(liste, operation):
-    return [operation(x) for x in liste]
-
-
-def double(x):
-    return x * 2
-
-
-def carre(x):
-    return x**2
-
-#utilisation
-liste = [1,2,3,4,5]
-resultat = appliquer_operation(liste,carre)
-print(f"Résultat : {resultat}")
-
-# (4) fonction lamda = anonyme , lambda argument : expression
-nombres = [1,2,3,4,5]
-print(f"double = {appliquer_operation(nombres,lambda x : x * 2)}")
-print(f"carre = {appliquer_operation(nombres,lambda x : x ** 2)}")
+#Exercice 3 : Configurateur de produit avec **kwargs et combinaison d’arguments
