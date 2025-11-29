4eeafc98866ada0376e77dfcff587a471da4551f
hyna42 <bylo@duck.com>
2025-11-29 14:01:58 +0100

    chap1:exo2

--- main.py
+++ main.py
@@ -1,47 +1,35 @@
-
 name = "amadou"
 print(f"Hello {name}")
-print("*"*50)
-
-#Exercice 1 : Calculateur de tarif de cinéma
-#Objectif : Créez un programme qui calcule le prix d’une place de cinéma en fonction de l’âge du spectateur.
+print("*" * 50)
 
 
-def calculer_tarif(age):
-    """calcule le tarif d'une place de cinéma selon l'âge"""
-    if age <= 5:
-        return 0
-    elif age <= 17:
-        return 7
-    elif age <= 64:
-        return 12
-    else:
-        return 8
-
-print(f"3 ans : {calculer_tarif(3)}€")
-print(f"12 ans : {calculer_tarif(12)}€")
-print(f"63 ans : {calculer_tarif(63)}€")
-print(f"70 ans : {calculer_tarif(70)}€")
+# Exercice 2 : Évaluateur de mot de passe
+# Objectif : créer un programme qui évalue la force d'un mot de passe
 
 
-
-
-print("*"*10+" match-case "+"*"*10)
-def calculer_tarif_bis(age):
-    """calcule le tarif d'une place de cinéma selon l'âge via match-case"""
-    match age:
-        case age if age<= 5:
-            return 0
-        case age if age<= 17:
-            return 7
-        case age if age<= 64:
-            return 12
+def evaluer_mot_de_passe(mot_de_passe):
+    len_mp = len(mot_de_passe)
+    match len_mp:
+        case n if n < 6:
+            return "Très faible"
+        case n if n <= 7:
+            return "Faible"
+        case n if n <= 11:
+            return "Moyen"
         case _:
-            return 8
+            return "Fort"
 
-        
-print(f"3 ans : {calculer_tarif_bis(3)}€")
-print(f"12 ans : {calculer_tarif_bis(12)}€")
-print(f"63 ans : {calculer_tarif_bis(63)}€")
-print(f"70 ans : {calculer_tarif_bis(70)}€")
-    
+
+def est_securise(mot_de_passe):
+    return "Sécurisé" if len(mot_de_passe) >= 8 else "Non sécurisé"
+
+
+print(f"Sécurité du mot de passe 'test' = {evaluer_mot_de_passe("test")}")
+print(f"Sécurité du mot de passe '1azerty' = {evaluer_mot_de_passe("1azerty")}")
+print(f"Sécurité du mot de passe '1234azerty!' = {evaluer_mot_de_passe("1234azerty!")}")
+print(
+    f"Sécurité du mot de passe '123azertyoihidniohi' = {evaluer_mot_de_passe("123azertyoihidniohi")}"
+)
+
+print(f"'azerty' => {est_securise("azerty")}")
+print(f"'1234azerty' => {est_securise("1234azerty")}")
