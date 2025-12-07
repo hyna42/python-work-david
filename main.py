6a20bb68b9a1cfa0fd1395de58f5b60022f65c47
hyna42 <bylo@duck.com>
2025-12-07 21:38:00 +0100

    feat(c2-decorateur): basic exemple

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-print("*"*60)
+# .\Scripts\activate
+# deactivate
+
+print("*" * 60)
 print("LES DECORATEUR".center(60))
-print("*"*60)
+print("*" * 60)
 ############################################################################
 # DECORATEURS
 ############################################################################
 
+def mon_decorateur(fonction):
+    def wrapper():
+        print("Quelque chose avant l'exécution")
+        result = fonction()
+        print("Quelque chose après l'exécution")
+        return result
+    return wrapper
+
+
+@mon_decorateur
+def dire_bonjour():
+    print("Bonjour")
+
+dire_bonjour()
